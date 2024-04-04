806130862a263b594aed794a2662cdc50325346c
moulineE <elmahdi.mouline@live.fr>
2024-04-04 10:22:44 +0000

    pycodestyle fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -10,6 +10,7 @@ def status():
     """ Status """
     return jsonify({"status": "OK"})
 
+
 @app_views.route('/languages_by_id/<lang_id>', strict_slashes=False)
 def languages_by_id(lang_id):
     """ languages by id """
